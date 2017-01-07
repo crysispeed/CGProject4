f705546ecc58ad9d77e876b9177adaba30ddbd2d
crysispeed <kcaglar.guler@tedu.edu.tr>
2017-01-07 22:46:42 +0200

    Add files via upload

---
+++ CGProject4/src/org/newdawn/spaceinvaders/NanoTimer.java
@@ -0,0 +1,25 @@
+package org.newdawn.spaceinvaders;
+
+
+public class NanoTimer implements NativeTimer
+{
+    public boolean available()
+    {
+        String version = System.getProperty("java.vm.version");
+        String sub = version.substring(version.indexOf(".")+1);
+        
+        if(Integer.parseInt(sub.charAt(0)+"") >= 5) return true;
+        
+        return false;
+    }
+    
+    public long getResolution()
+    {
+        return 1000000000L;
+    }
+    
+    public long getClockTicks()
+    {
+        return System.nanoTime();
+    }
+}
